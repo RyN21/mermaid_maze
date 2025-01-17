a03152887138c47ed1c4b5ea1d0b5d73196fd137
Ryan Laleh <33795357+RyN21@users.noreply.github.com>
2025-01-16 22:06:01 -0700

    Setup possible architecture for game

---
+++ lib/game_window.rb
@@ -0,0 +1,14 @@
+
+class GameWindow < Gosu::Window
+
+  def initialize
+    super 800, 600, fullscreen: false
+    self.caption = "Critter Quest"
+  end
+
+  def update
+  end
+
+  def draw
+  end
+end
