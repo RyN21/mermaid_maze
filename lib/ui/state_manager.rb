b01a53745e51e2dc596aed1f9bea81e9c1fc262a
Ryan Laleh <33795357+RyN21@users.noreply.github.com>
2025-02-03 18:54:27 -0700

    Add state manager class to handle switching between window states

---
+++ lib/ui/state_manager.rb
@@ -0,0 +1,26 @@
+require "gosu"
+require_relative "menu"
+require_relative "game_window"
+
+class StateManager
+  def initialize(window)
+    @window        = window
+    @current_state = nil
+  end
+
+  def switch_do(state)
+    @current_state = state
+  end
+
+  def update
+    @current_state.update if @current_state
+  end
+
+  def draw
+    @current_state.draw if @current_state
+  end
+
+  def button_down(id)
+    @current_state.button_down(id) if @current_state
+  end
+end
