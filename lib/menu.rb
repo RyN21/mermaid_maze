db359e3dae417fddcf20b336dc29d519bc7cdf73
Ryan Laleh <33795357+RyN21@users.noreply.github.com>
2025-02-04 20:55:24 -0700

    Edit menu screen

--- lib/menu.rb
+++ lib/menu.rb
@@ -9,8 +9,8 @@ class Menu
     @mermaid_purple = Config::MERMAIDS[:purple][:down][0]
     @mermaid_pink   = Config::MERMAIDS[:pink][:down][0]
     @mermaid_green  = Config::MERMAIDS[:green][:down][0]
-    # @background = Gosu::Image.new("assets/images/background.png")
-    # @logo = Gosu::Image.new("assets/images/logo.png")
+    @background_image   = Gosu::Image.new("assets/images/background_ocean.jpg")
+    @logo = Gosu::Image.new("assets/images/logo.png")
     @font = Gosu::Font.new(30)
     # @menu_music = Gosu::Song.new("assets/sounds/menu_music.mp3")
     # @menu_music.play(true)
@@ -19,17 +19,21 @@ class Menu
   def update; end
 
   def draw
-    @font.draw_text("SELECT CHARACTER", 263, 200, 0, 1, 1)
+    # @background_image.draw 0, 0, 0, 0.20, 0.20
+
+    @font.draw_text("SELECT CHARACTER", 263, 250, 0, 1, 1)
     CHARACTERS.each_with_index do |char, index|
 
       shift = index * 200
-      color = index == @character_index ? Gosu::Color::YELLOW : Gosu::Color::WHITE
+      color = index == @character_index ? Gosu::Color.argb(0xff_ff00ff) : Gosu::Color::WHITE
       @font.draw_text(char, 60 + shift, 430, 1, 1, 1, color)
     end
     @mermaid_green.draw(47, 300, 0, 2, 2)
     @mermaid_pink.draw(247, 300, 0, 2, 2)
     @mermaid_purple.draw(447, 300, 0, 2, 2)
     @mermaid_blue.draw(647, 300, 0, 2, 2)
+    @logo.draw(200, 50, 0, 1, 1)
+    @font.draw_text("PRESS ENTER TO PLAY", 250, 525, 0, 1, 1)
     # @background.draw(0, 0, 0, 1, 1)
     # @logo.draw(0, 0, 0, 1, 1)
   end
